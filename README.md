19e343b75df207c9ec3684e3415e1a5476a6373c
Bijaelo <oliver.p.madsen@gmail.com>
2020-09-21 15:05:17 +0200

    added compute S3 etag in R

---
+++ README.md
@@ -0,0 +1,1 @@
+"#Stackoverflow answers" "This repo contains contigous answers to various stackoverflow questions that were too long to post in a single stackoverflow answer." 
---
+++ S3/compute Etag in aws S3 in R/compute etag in aws s3 in r.R
@@ -0,0 +1,45 @@
+
+
+# Set region and connection variable
+region <- ''
+pkey <- ''
+psecret <- ''
+pbucket <- ''
+
+calculate_s3_etag <- function(file_path, chunk_size = 100 * 1024**2){
+  require(digest)
+  # Open and read file
+  f <- file(file_path, 'rb')
+  on.exit(close(f))
+  md5 <- list()
+  while(!identical((r <- readBin(f, raw(), chunk_size)), raw())){
+    md5 <- c(md5, list(r))
+  }
+  md5 <- unlist(md5)
+  list('etag' = digest::digest(as.raw(md5), 'md5', serialize = FALSE),
+       'size' = length(md5))
+}
+# Import tag from S3
+if(key != ''){
+  head <- head_object(object = obj,
+                      bucket = pbucket,
+                      key = key,
+                      secret = secret,
+                      region = region)
+}else{
+  # If no one has a key, use known value from own S3 bucket.
+  head <- ''
+  attr(head, 'etag') <- "d8c329c9d51543437ebe20e5a02b834e"
+  attr(head, 'size') <- 221770
+}
+calc <- calculate_s3_etag('daily.parquet')
+tag <- calc$etag
+size <- calc$size
+cat('Tag identical? - \n', identical(tag,
+                                     # tag taken from S3 head.
+                                     gsub('\\"', '', attr(head, 'etag'))), '\n')
+
+cat('Size identical? - \n', identical(size,
+                                     # tag taken from S3 head.
+                                     as.integer(attr(head, 'content-length'))), '\n')
+
